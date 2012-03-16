90ca991c74dd4928831283a2d698ff16b7134ba7
Ethan Burns <burns.ethan@gmail.com>
2012-03-16 10:08:12 -0400

    wgen: initialize to the middle height and grow both up and down.

--- wgen/wgen.go
+++ wgen/wgen.go
@@ -9,17 +9,13 @@ import (
 )
 
 const (
-	// The mean of the distribution over the
+	// The parameters of the distribution over the
 	// number of Gaussian2ds.
-	meanGauss = 200
+	meanGauss, stdevGauss = 180, 20
 
-	// Standard deviation of the distribution
-	// over the number of Gaussian2ds.
-	stdevGauss = 20
-
-	// The rate parameter of the exponential
-	// distribution of mountain heights.
-	rateHeight = 1.1
+	// The parameters of the normal distribution
+	// over mountain growths.
+	meanGrowth, stdevGrowth = 0, world.MaxHeight/4
 
 	// Minimum and maximum Gaussian2d covariance of
 	// random Gaussian2ds.
@@ -28,7 +24,7 @@ const (
 	// Minimum and maximum standard deviation of
 	// random Gaussian2ds as a factor of the
 	// map width and height.
-	sdevMin, sdevMax = 0.04, 0.07
+	sdevMin, sdevMax = 0.04, 0.1
 )
 
 var (
@@ -52,11 +48,20 @@ func main() {
 		for j := 0; j < w.H; j++ {
 			l := w.At(i, j)
 			l.Terrain = &world.Terrain['g']
+			l.Height = world.MaxHeight / 2
 		}
 	}
 
 	ch := make(chan *Gaussian2d)
-	go genGaussian2ds(&w, ch)
+	go func() {
+		num := int(rand.NormFloat64() * stdevGauss + meanGauss)
+		fmt.Println("num gaussians: ", num)
+		for i := 0; i < num; i++ {
+			ch <- randomGaussian2d(&w)
+		}
+		close(ch)
+	}()
+
 	for g := range ch {
 		grow(&w, g)
 	}
@@ -73,7 +78,7 @@ func grow(w *world.World, g *Gaussian2d) {
 	xmin, xmax := int(g.Mx - s*g.Sx), int(g.Mx + s*g.Sx)
 	ymin, ymax := int(g.My - s*g.Sy), int(g.My + s*g.Sy)
 
-	ht := rndHt()
+	ht := int(rand.NormFloat64()*stdevGrowth + meanGrowth)
 	peek := g.PDF(g.Mx, g.My)
 	mul := float64(ht) / peek
 
@@ -92,43 +97,6 @@ func grow(w *world.World, g *Gaussian2d) {
 	}
 }
 
-// rndHt returns a randomly choosen height for a
-// mountain.  Heights are choosen from an exponential
-// distribution.
-func rndHt() int {
-	ht := rand.ExpFloat64()/1.1 + 1
-
-	if ht > world.MaxHeight {
-		ht = world.MaxHeight
-	}
-
-	return int(ht)
-}
-
-// genGaussian2ds generates a bunch of random
-// Gaussian2ds and sends them to the given channel.
-func genGaussian2ds(w *world.World, ch chan<-*Gaussian2d) {
-	num := int(rand.NormFloat64() * stdevGauss + meanGauss)
-	fmt.Println("num gaussians: ", num)
-
-	gs := make([]*Gaussian2d,0, num)
-	for i := 0; i < num; i++ {
-		tries := 0
-retry:
-		g := randomGaussian2d(w)
-		for j := range gs {
-			if tries < 1000 && near(g, gs[j]) {
-				tries++
-				goto retry
-			}
-		}
-
-		gs = append(gs, g)
-		ch <- g
-	}
-	close(ch)
-}
-
 // randomGaussian2d creates a random Gaussian2d
 // somewhere in the world and returns it.
 func randomGaussian2d(w *world.World) *Gaussian2d {
@@ -143,22 +111,5 @@ func randomGaussian2d(w *world.World) *Gaussian2d {
 	cov := rand.Float64() * (covMax - covMin) + covMin
 
 	return NewGaussian2d(mx, my, sx, sy, cov)
-}
-
-// near returns true if the two Gassians are too close.
-func near(a, b *Gaussian2d) bool {
-	const s = 2	// standard deviations to grow around
-	axmin, axmax := int(a.Mx - s*a.Sx), int(a.Mx + s*a.Sx)
-	bxmin, bxmax := int(b.Mx - s*b.Sx), int(b.Mx + s*b.Sx)
-	if axmin > bxmax || bxmin > axmax {
-		return false
-	}
-
-	aymin, aymax := int(a.My - s*a.Sy), int(a.My + s*a.Sy)
-	bymin, bymax := int(b.My - s*b.Sy), int(b.My + s*b.Sy)
-	if aymin > bymax || bymin > aymax {
-		return false
-	}
-	return true
 }
 
