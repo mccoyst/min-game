090c0ff77ecb86b623293a6ed5aac0235a942608
Steve McCoy <mccoyst@gmail.com>
2012-03-17 17:30:20 -0400

    Add some basic tiles.

---
+++ wgen/gaussian.go
@@ -0,0 +1,66 @@
+package main
+
+import "math"
+
+// Gaussian2d is a 2-dimensional normal distribution.
+type Gaussian2d struct {
+	Mx, My float64 // mean
+	Sx, Sy float64 // standard deviation
+	Cov    float64 // covariance
+	Ht     float64 // value at the mean
+
+	// pre-computed values for the PDF
+	covcov, cov2, sxsy, sxsx, sysy, coeff, expcoeff float64
+}
+
+// NewGaussian2d returns a new 2-dimensional Gaussian2d with
+// the given mean, standard deviation, covariance
+//
+// A bunch of values used during PDF computation are
+// pre-computed for efficiency, so if any of the exported
+// fields are changed then Precompute() must be called
+// to re-compute these fields.
+func NewGaussian2d(mx, my, sx, sy, ht, cov float64) *Gaussian2d {
+	g := &Gaussian2d{
+		Mx:  mx,
+		My:  my,
+		Sx:  sx,
+		Sy:  sy,
+		Cov: cov,
+		Ht:  ht,
+	}
+	g.Precompute()
+	return g
+}
+
+// precompute pre-computes a bunch of information
+// that is used during PDF computation.  It must
+// be called if any of the exported fields of
+// the gaussion are changed after NewGaussian2d
+// is called.
+func (g *Gaussian2d) Precompute() {
+	sxsy := g.Sx * g.Sy
+	covcov := g.Cov * g.Cov
+	g.covcov = covcov
+	g.cov2 = g.Cov * 2
+	g.sxsy = sxsy
+	g.sxsx = g.Sx * g.Sx
+	g.sysy = g.Sy * g.Sy
+	g.coeff = 1 / (2 * math.Pi * sxsy * math.Sqrt(1-covcov))
+	g.expcoeff = -0.5 * (1 - covcov)
+
+	// Re-compute coefficient so that the
+	// mean probability is g.Ht 
+	m := g.PDF(g.Mx, g.My)
+	g.coeff = (g.Ht / m) / (2 * math.Pi * sxsy * math.Sqrt(1-covcov))
+}
+
+// PDF returns the probability density.
+func (g *Gaussian2d) PDF(x, y float64) float64 {
+	devx := x - g.Mx
+	devy := y - g.My
+	vl := devx * devx / g.sxsx
+	vl += devy * devy / g.sysy
+	vl -= g.cov2 * devx * devy / g.sxsy
+	return g.coeff * math.Exp(g.expcoeff*vl)
+}
---
+++ world/terrain.go
@@ -0,0 +1,26 @@
+package world
+
+import (
+	"image/color"
+)
+
+// TerrainType holds information on a given type of
+// terrain.
+type TerrainType struct {
+	Char  uint8       // character representation
+	Color color.RGBA // color at the highest altitude
+}
+
+// Terrain is an array with the canonical terrain
+// representations, indexed by the terrain type's
+// unique character.
+var Terrain = []TerrainType{
+	// Grass-land
+	int('g'): {'g', color.RGBA{0, 255, 0, 255}},
+	// Mountain
+	int('m'): {'m', color.RGBA{128, 128, 128, 255}},
+	// Water
+	int('w'): {'w', color.RGBA{0, 0, 255, 255}},
+	// Lava
+	int('l'): {'l', color.RGBA{255, 0, 0, 255}},
+}
