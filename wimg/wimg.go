a78faeb3693e3dff12f0bedbf22963d5e28e6833
Ethan Burns <burns.ethan@gmail.com>
2012-03-19 19:32:30 -0400

    wgen: new technique for water generation. Flood some local minima until there is enough water.

--- wimg/wimg.go
+++ wimg/wimg.go
@@ -3,9 +3,6 @@ package main
 import (
 	"bufio"
 	"flag"
-	"image"
-	"image/color"
-	"image/png"
 	"minima/world"
 	"os"
 )
@@ -13,7 +10,7 @@ import (
 var (
 	outFile = flag.String("o", "world.png", "The output file")
 	echo    = flag.Bool("e", false, "Echo the world to standard output")
-	blkSize = flag.Int("s", 1, "Size of each block in pixels")
+	depth = flag.Bool("d", false, "Draw water depth")
 )
 
 func main() {
@@ -24,12 +21,9 @@ func main() {
 		panic(err)
 	}
 
-	imgout, err := os.Create(*outFile)
-	if err != nil {
+	if err := w.SavePng(*outFile, *depth); err != nil {
 		panic(err)
 	}
-	defer imgout.Close()
-	png.Encode(imgout, (*worldImg)(&w))
 
 	if *echo {
 		out := bufio.NewWriter(os.Stdout)
@@ -40,35 +34,3 @@ func main() {
 	}
 }
 
-type worldImg world.World
-
-// Bounds implements the Bounds() method of
-// the image.Image interface.
-func (w *worldImg) Bounds() image.Rectangle {
-	return image.Rect(0, 0, w.W*(*blkSize), w.H*(*blkSize))
-}
-
-// At implements the At() method of the
-// image.Image interface.
-func (w *worldImg) At(x, y int) color.Color {
-	x /= *blkSize
-	y /= *blkSize
-	loc := (*world.World)(w).At(x, y)
-	f := float64(loc.Height+world.MaxHeight) / (2 * world.MaxHeight)
-	if f > 1 {
-		panic("Color factor is >1 in worldImg.At")
-	}
-	c := loc.Terrain.Color
-	return color.RGBA{
-		R: uint8(float64(c.R) * f),
-		G: uint8(float64(c.G) * f),
-		B: uint8(float64(c.B) * f),
-		A: c.A,
-	}
-}
-
-// ColorModel implements the ColorModel() method
-// of the image.Image interface.
-func (w *worldImg) ColorModel() color.Model {
-	return color.RGBAModel
-}
