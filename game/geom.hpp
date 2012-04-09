f0920ce61ef34bed1192d7df34c5091b73abe8e1
Ethan Burns <burns.ethan@gmail.com>
2012-04-09 15:32:08 -0400

    Remove a bunch of crappy bbox code.

--- game/geom.hpp
+++ game/geom.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "world.hpp"
+#include <vector>
 #include <cassert>
 
 // An Isection holds information about an intersection.
@@ -30,90 +31,20 @@ struct Isection {
 // A Bbox is a bounding box that can be used for collision
 // detection, among other things.
 struct Bbox {
-	Vec2 min, max;
+	Vec2 pt, sz;
 
 	// Bbox constructs a new bounding box with the given
-	// minimum and maximum points.
-	Bbox(const World &w, const Vec2 &mn, const Vec2 &mx) : min(mn), max(mx) {
-		if (min.x > max.x) {
-			Fixed t = max.x;
-			max.x = min.x;
-			min.x = t;
-		}
-		if (min.y > max.y) {
-			Fixed t = max.y;
-			max.y = min.y;
-			min.y = t;
-		}
-		normalize(w);
-	}
+	// lower left corner and size.
+	Bbox(const Vec2 &p, const Vec2 &s) : pt(p), sz(s) { }
 
 	// Isect returns the intersection between the two bounding boxes.
-	Isection Isect(const World &w, const Bbox &o) const {
-		assert (min < w.size);
-		assert (max >= Vec2::Zero);
-		assert (o.min < w.size);
-		assert (o.max >= Vec2::Zero);
-
-		Vec2 overlap(Fixed(-1), Fixed(-1));
-		if (Fixed::Between(o.min.x, o.max.x, max.x))
-			overlap.x = max.x - o.min.x;
-		else if (Fixed::Between(min.x, max.x, o.max.x))
-			overlap.x = o.max.x - min.x;
-		else if (max.x >= w.size.x && o.max.x < w.size.x &&
-			Fixed::Between(o.min.x, o.max.x, max.x % w.size.x))
-			overlap.x = o.max.x - max.x % w.size.x;
-		else if (o.max.x >= w.size.x && max.x < w.size.x &&
-			Fixed::Between(min.x, max.x, o.max.x % w.size.x))
-			overlap.x = o.max.x - min.x % w.size.x;
-
-		if (Fixed::Between(o.min.y, o.max.y, max.y))
-			overlap.y = max.y - o.min.y;
-		else if (Fixed::Between(min.y, max.y, o.max.y))
-			overlap.y = o.max.y - min.y;
-		else if (max.y >= w.size.y && o.max.y < w.size.y &&
-			Fixed::Between(o.min.y, o.max.y, max.y % w.size.y))
-			overlap.y = max.y - o.min.y % w.size.y;
-		else if (o.max.y >= w.size.y && max.y < w.size.y &&
-			Fixed::Between(min.y, max.y, o.max.y % w.size.y))
-			overlap.y = o.max.y - max.y % w.size.y;
-
-		return Isection(overlap);
+	Isection Isect(const Bbox &o) const {
+		return Isection();
 	}
 
 	// Move moves the bounding box, wrapping it so that the minimum
 	// point is always within the world's (0,0)--(width-1,height-1).
-	void Move(const World &w, const Vec2 &d) {
-		min += d;
-		max.x += d.x;
-		normalize(w);
-	}
-
-private:
-
-	// normalize ensures that the minimum coordinate is
-	// within the (0,0)--w.size while the maximum coordinate
-	// is allowed to pass off of the end of the world.
-	void normalize(const World &w) {
-		if (min.x >= w.size.x) {
-			Fixed width = max.x - min.x;
-			min.x %= w.size.x;
-			max.x = min.x + width;
-		} else if (min.x < Fixed(0)) {
-			Fixed width = max.x - min.x;
-			min.x = w.size.x + (min.x % w.size.x);
-			max.x = min.x + width;
-		}
-
-		if (min.y >= w.size.y) {
-			Fixed height = max.y - min.y;
-			min.y %= w.size.y;
-			max.y = min.y + height;
-		} else if (min.y < Fixed(0)) {
-			Fixed height = max.y - min.y;
-			min.y = w.size.y + (min.y % w.size.y);
-			max.y = min.y + height;
-		}
-		assert (min < w.size);
+	void Move(const Vec2 &d) {
+		pt += d;
 	}
 };
