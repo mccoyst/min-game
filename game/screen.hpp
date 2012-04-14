26f0750a14a1e31dbbe56d94bf1bdd3e63a3280d
Ethan Burns <burns.ethan@gmail.com>
2012-04-14 10:27:35 -0400

    Clear() and Flip() are called from the screen and not the screen stack.

--- game/screen.hpp
+++ game/screen.hpp
@@ -10,22 +10,15 @@ struct ScreenStack {
 	void Run();
 	void Push(std::shared_ptr<Screen>);
 	void Pop();
-	void SetDrawFps(bool);
-	
-	
+
 	enum {
 		// FrameMsec is the minimum frame time in msec.
 		FrameMsec = 20,
-	
-		// FpsTime is the time in msec between redrawing the
-		// frame rate.
-		FpsTime = 500,
 	};
 
 private:
 	std::vector< std::shared_ptr<Screen> > stk;
 	std::shared_ptr<Ui> win;
-	bool drawFps;
 	unsigned long nFrames;
 	double meanFrame;
 };
@@ -35,8 +28,7 @@ struct Screen {
 
 	virtual void Update(ScreenStack&) = 0;
 
-	// Draw draws the screen.  No need to call Clear() or Flip()
-	// on the Ui, the screen stack does this for you.
+	// Draw draws the screen.
 	virtual void Draw(std::shared_ptr<Ui>) = 0;
 
 	virtual void Handle(ScreenStack&, Event&) = 0;
