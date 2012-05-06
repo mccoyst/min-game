f6e4cf4a3d8543ab48c808258cfb7acb5742f3ff
Ethan Burns <burns.ethan@gmail.com>
2012-05-06 11:23:54 -0400

    wimg: mountains are brown.

--- world/terrain.go
+++ world/terrain.go
@@ -18,7 +18,7 @@ type TerrainType struct {
 // unique character.
 var Terrain = []TerrainType{
 	int('g'): {'g', "grass", color.RGBA{0, 255, 0, 255}},
-	int('m'): {'m', "mountain", color.RGBA{196, 196, 196, 255}},
+	int('m'): {'m', "mountain", color.RGBA{196, 128, 0, 255}},
 	int('w'): {'w', "water", color.RGBA{0, 0, 255, 255}},
 	int('l'): {'l', "lava", color.RGBA{255, 0, 0, 255}},
 	int('d'): {'d', "desert", color.RGBA{255, 255, 0, 255}},
