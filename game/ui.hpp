ace93522b96c467e13f1783ef49da2af23b86c36
Steve McCoy <mccoyst@gmail.com>
2012-05-15 12:44:46 -0400

    Names starting with underscores are reserved for the implementation.

--- game/ui.hpp
+++ game/ui.hpp
@@ -78,8 +78,8 @@ public:
 };
 
 struct Color {
-	Color(unsigned char _r, unsigned char _g, unsigned char _b,
-		unsigned char _a = 255) : r(_r), g(_g), b(_b), a(_a) { }
+	Color(unsigned char r, unsigned char g, unsigned char b,
+		unsigned char a = 255) : r(r), g(g), b(b), a(a) { }
 	unsigned char r, g, b, a;
 };
 
