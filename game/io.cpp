026cce25d0c64d091a253e820ce457c96dcc4659
Steve McCoy <mccoyst@gmail.com>
2012-05-30 16:31:48 -0400

    Of course I forgot these vital files.

---
+++ game/io.cpp
@@ -0,0 +1,32 @@
+// Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
+
+#include "io.hpp"
+#include <iostream>
+
+std::ostream &cout(){
+	return std::cout;
+}
+
+std::ostream &cerr(){
+	return std::cerr;
+}
+
+int printf(std::ostream &out, const std::string &fmt){
+	for(auto c = fmt.begin(), end = fmt.end(); c != end; c++){
+		if(*c != '%'){
+			out << *c;
+			continue;
+		}
+
+		c++;
+		if(c == end)
+			out << "[invalid format string \"" << fmt << "\"]";
+		else if(*c == '%')
+			out << '%';
+		else if(*c == 'v')
+			out << "[no more arguments for format \"" << *c << "\"]";
+		else
+			out << "[invalid format specifier \"" << *c << "\"]";
+	}
+	return 0;
+}
---
+++ game/io.hpp
@@ -0,0 +1,32 @@
+// Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
+#pragma once
+
+#include <ostream>
+#include <string>
+
+std::ostream &cout();
+std::ostream &cerr();
+
+int printf(std::ostream &out, const std::string &fmt);
+
+template<class Arg, class... Args>
+int printf(std::ostream &out, const std::string &fmt, const Arg &arg, const Args&... args){
+	auto c = fmt.begin();
+	auto end = fmt.end();
+	while(c != end && *c != '%'){
+		out << *c;
+		c++;
+	}
+
+	c++;
+	if(c == end)
+		out << "[invalid format string \"" << fmt << "\"]";
+	else if(*c == '%')
+		out << '%';
+	else if(*c == 'v')
+		out << arg;
+	else
+		out << "[invalid format specifier \"" << *c << "\"]";
+	c++;
+	return 1 + printf(out, fmt.substr(c-fmt.begin()), args...);
+}
