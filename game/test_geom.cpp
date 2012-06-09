6f65ece0fac9465d56230903d627b9f28a833655
Steve McCoy <mccoyst@gmail.com>
2012-06-09 14:58:48 -0400

    More tests.

--- game/test_geom.cpp
+++ game/test_geom.cpp
@@ -10,4 +10,13 @@ void test_bbox(Testo &t){
 	Bbox b{ Vec2{}, Vec2{Fixed{5}, Fixed{5}} };
 	Isection self = b.Isect(b);
 	t.Assert(self, "self");
+	t.Assert(self.overlap.x == b.sz.x, "self.x");
+	t.Assert(self.overlap.y == b.sz.y, "self.y");
+
+	Bbox c{ Vec2{Fixed{1}, Fixed{1}}, Vec2{Fixed{5}, Fixed{5}} };
+	Isection bc = b.Isect(c);
+	Isection cb = c.Isect(b);
+	t.Assert(bc, "bc");
+	t.Assert(cb, "cb");
+	t.Assert(bc == cb, "bc == cb");
 }
