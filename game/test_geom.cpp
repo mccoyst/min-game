29dbc5d12dbf2d1c752ad1f8f3cf2dcf66cd2eca
Steve McCoy <mccoyst@gmail.com>
2012-06-09 14:45:03 -0400

    Start testing geom.

---
+++ game/test_geom.cpp
@@ -0,0 +1,11 @@
+// Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
+
+#include "geom.hpp"
+
+void test_isection(Testo &t){
+	t.Assert(Isection{} == false, "default");
+}
+
+void test_bbox(Testo &t){
+
+}
