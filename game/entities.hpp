82c3e186b36c4d0da251aea66d5f56f29ed7fe35
Steve McCoy <mccoyst@gmail.com>
2012-06-10 02:34:37 -0400

    I always forget to add files, apparently.

---
+++ game/entities.hpp
@@ -0,0 +1,21 @@
+// Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
+#pragma once
+
+#include "geom.hpp"
+
+class Ui;
+
+class Astro{
+	Bbox box;
+	Img *sprite;
+public:
+	Astro(Img*);
+	Astro(const Astro&) = default;
+
+	Bbox Box() const;
+	void Move(); //TODO: take the local landscape
+	void MoveTo(Vec2);
+	void Draw(Ui&) const;
+
+	Vec2 vel;
+};
---
+++ game/player.cpp
@@ -0,0 +1,24 @@
+// Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
+
+#include "entities.hpp"
+
+Astro::Astro(Img *i)
+	: box(Vec2{Fixed{}, Fixed{}}, Vec2{Fixed{16},Fixed{16}}),
+	sprite(i){
+}
+
+Bbox Astro::Box() const{
+	return box;
+}
+
+void Astro::Move(){
+	box.Move(vel);
+}
+
+void Astro::MoveTo(Vec2 pos){
+	box.min = pos;
+}
+
+void Astro::Draw(Ui &ui) const{
+	ui.DrawCam(box.min, sprite);
+}
