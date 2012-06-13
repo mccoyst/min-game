e00cc967919390b05c16a2836043cbbc1319ca21
Steve McCoy <mccoyst@gmail.com>
2012-06-13 00:05:28 -0400

    IWYU 3, also don't try to inline such long functions.

--- game/geom.hpp
+++ game/geom.hpp
@@ -1,7 +1,10 @@
 // Copyright © 2012 the Minima Authors under the MIT license. See AUTHORS for the list of authors.
 #pragma once
 
-#include "ui.hpp"
+#include "fixed.hpp"
+
+struct Color;
+class Ui;
 
 // An Isection holds information about an intersection.
 class Isection {
@@ -47,46 +50,11 @@ public:
 	Bbox(const Vec2 &p, const Vec2 &s) : min(p), sz(s) { }
 
 	// Isect returns the intersection between the two bounding boxes.
-	Isection Isect(const Bbox &o) const {
-		Isection isect;
-		Vec2 max = min + sz, omax = o.min + o.sz;
-		if (max.x >= o.min.x && max.x <= omax.x)
-			isect.overlap.x = max.x - o.min.x;
-		else if (omax.x >= min.x && omax.x <= max.x)
-			isect.overlap.x = omax.x - min.x;
-
-		if (max.y >= o.min.y && max.y <= omax.y)
-			isect.overlap.y = max.y - o.min.y;
-		else if (omax.y >= min.y && omax.y <= max.y)
-			isect.overlap.y = omax.y - min.y;
-
-		return isect;
-	}
+	Isection Isect(const Bbox &o) const;
 
 	// IsectWorld returns the intersection between two bounding
 	// boxes that may be wrapping around the edge of the world.
-	Isection IsectWorld(Vec2 size, Bbox &o) {
-		if (!Wraps(size) && !o.Wraps(size))
-			return Isect(o);
-
-		if (o.Wraps(size)) {
-			o.WrapMin(size);
-			Isection isect = Isect(o);
-			if (isect)
-				return isect;
-	
-			o.WrapMax(size);
-			return Isect(o);
-		}
-
-		WrapMin(size);
-		Isection isect = Isect(o);
-		if (isect)
-			return isect;
-
-		WrapMax(size);
-		return Isect(o);
-	}
+	Isection IsectWorld(Vec2 size, Bbox &o);
 
 	// Move moves the bounding box.
 	void Move(const Vec2 &d) {
@@ -96,61 +64,22 @@ public:
 	// WrapMin wraps the bounding box's point so that it's
 	// minimum point is within the rectangle defined by the
 	// point 0,0 and the given size.
-	void WrapMin(const Vec2 &sz) {
-		if (min >= Vec2{} && min < Vec2{} + sz)
-			return;
-
-		if (min.x < Fixed(0))
-			min.x = sz.x - (-min.x % sz.x);
-		else if (min.x >= sz.x)
-			min.x %= sz.x;
-
-		if (min.y < Fixed(0))
-			min.y = sz.y - (-min.y % sz.y);
-		else if (min.y >= sz.y)
-			min.y %= sz.y;
-	}
+	void WrapMin(const Vec2 &sz);
 
 	// WrapMax wraps the bounding box's point so that it's
 	// maximum point is within the rectangle defined by the
 	// point 0,0 and the given size.
-	void WrapMax(const Vec2 &sz) {
-		Vec2 max = min + sz;
-
-		if (max >= Vec2{} && max < Vec2{} + sz)
-			return;
-
-		if (max.x < Fixed(0))
-			max.x = sz.x - (-max.x % sz.x);
-		else if (max.x >= sz.x)
-			max.x %= sz.x;
-
-		if (max.y < Fixed(0))
-			max.y = sz.y - (-max.y % sz.y);
-		else if (max.y >= sz.y)
-			max.y %= sz.y;
-
-		min = max - sz;
-	}
+	void WrapMax(const Vec2 &sz);
 
 	// Wraps returns true if the given bounding box would wrap
 	// around the rectangle defined by 0,0 and the given size.
-	bool Wraps(const Vec2 &sz) const {
-		if (min < Vec2{} || min >= Vec2{} + sz)
-			return true;
-		Vec2 max = min + sz;
-		return max < Vec2{} && max >= Vec2{} + sz;
-	}
+	bool Wraps(const Vec2 &sz) const;
 
 	// Draw draws the bounding box outline.
-	void Draw(Ui &win, const Color& c) const {
-		win.DrawRect(min, sz, c);
-	}
+	void Draw(Ui &win, const Color& c) const;
 
 	// Fill draws the bounding box filled.
-	void Fill(Ui &win, const Color& c) const {
-		win.FillRect(min, sz, c);
-	}
+	void Fill(Ui &win, const Color& c) const;
 };
 
 inline bool operator == (const Bbox &a, const Bbox &b){
