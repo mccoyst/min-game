4c7e47555b2738a439c63911ad931d1bf0df66e9
Steve McCoy <mccoyst@gmail.com>
2012-11-12 09:02:12 -0500

    Animate the astro.

--- minima/player.go
+++ minima/player.go
@@ -10,6 +10,21 @@ import (
 type Player struct {
 	wo   *world.World
 	body Body
+	face, frame int
+	ticks int
+}
+
+const Tempo = 40
+var frames [][]ui.Rectangle
+
+func init() {
+	// TODO(mccoyst): Read this info from a file
+	for y := 0; y < 4; y++ {
+		frames = append(frames, make([]ui.Rectangle, 2))
+		for x := 0; x < 2; x++ {
+			frames[y][x] = ui.Rect(float64(x*TileSize), float64(y*TileSize), float64(x*TileSize + TileSize), float64(y*TileSize + TileSize))
+		}
+	}
 }
 
 func NewPlayer(wo *world.World, p ui.Point) *Player {
@@ -22,13 +37,36 @@ func NewPlayer(wo *world.World, p ui.Point) *Player {
 }
 
 func (p *Player) Move(w *world.World) {
+	p.ticks++
+	if p.ticks >= Tempo {
+		p.frame++
+		if p.frame >= 2 {
+			p.frame = 0
+		}
+		p.ticks = 0
+	}
+
+	// TODO(mccoyst): read from the same file, yadda yadda
+	if p.body.Vel.Y > 0 {
+		p.face = 0
+	}
+	if p.body.Vel.Y < 0 {
+		p.face = 3
+	}
+	if p.body.Vel.X > 0 {
+		p.face = 2
+	}
+	if p.body.Vel.X < 0 {
+		p.face = 1
+	}
+
 	p.body.Move(w)
 }
 
 func (p *Player) Draw(d Drawer, cam Camera) error {
 	_, err := cam.Draw(d, ui.Sprite{
 		Name:   "Astronaut",
-		Bounds: ui.Rect(0, 0, TileSize, TileSize),
+		Bounds: frames[p.face][p.frame],
 		Shade:  1.0,
 	}, p.body.Box.Min)
 	return err
