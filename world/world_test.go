da1a1e0af157087cfc8c7cba59155118634bf40e
Ethan Burns <burns.ethan@gmail.com>
2012-11-12 14:06:38 -0500

    Add some boring test to make sure world.wrap is correct. It was.

---
+++ world/world_test.go
@@ -0,0 +1,20 @@
+package world
+
+import (
+	"testing"
+)
+
+func TestWrap(t *testing.T) {
+	tests := [...]struct { x, bound, w int } {
+		{ 0, 500, 0 },
+		{ -1, 500, 499 },
+		{ -500, 500, 0 },
+		{ 500, 500, 0 },
+		{ 501, 500, 1 },
+	}
+	for _, tst := range tests {
+		if w := wrap(tst.x, tst.bound); w != tst.w {
+			t.Error("Expected wrap(%d, %d)=%d, got %d", tst.x, tst.bound, tst.w, w)
+		}
+	}
+}
