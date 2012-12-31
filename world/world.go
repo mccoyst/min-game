c5cf154de0f6af31c961509f76d825a04b2d62fd
Ethan Burns <burns.ethan@gmail.com>
2012-12-31 15:26:36 -0500

    Fix the world stuff since the merge didn't seem to work?

--- world/world.go
+++ world/world.go
@@ -146,8 +146,8 @@ func (w *World) Wrap(x, y int) (int, int) {
 }
 
 func (w *World) Tile(p geom.Point) (int, int) {
-	return wrap(int(math.Floor(p.X/TileSize.X)), w.W),
-		wrap(int(math.Floor(p.Y/TileSize.Y)), w.H)
+	return int(math.Floor(p.X / TileSize.X)),
+		int(math.Floor(p.Y / TileSize.Y))
 }
 
 // wrap returns the value of n wrapped around if it goes
